86d7e920789f2e3b54ce3ca08b2b8bcab7fbb23d
3bdoelnaggar <3bdoelnaggar@gmail.com>
2020-08-24 06:00:12 +0200

    add playstore link

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,10 +4,8 @@ import kotlinx.css.*
 import kotlinx.html.js.onClickFunction
 import react.dom.*
 import react.dom.h3
-import styled.css
-import styled.styledDiv
-import styled.styledH1
-import styled.styledImg
+import react.dom.p
+import styled.*
 
 fun main() {
     val root = document.getElementById("root")
@@ -17,7 +15,7 @@ fun main() {
             styledH1 {
                 css {
                     textAlign = TextAlign.center
-                    fontSize = 8.em
+                    fontSize = 6.em
                 }
                 +"Abdalla Elnaggar"
             }
@@ -25,55 +23,87 @@ fun main() {
             styledDiv {
                 css {
                     textAlign = TextAlign.center
-                    margin="10"
+                    margin = "10"
                 }
-                styledImg {
-                    css {
-                        margin="25px"
-                    }
-
+                a {
                     attrs {
-                        width = "100"
-                        height = "100"
-                        src = "./linkedin.png"
-                        onClickFunction = {
-                            window.alert("Twitter")
+                        href = "https://www.linkedin.com/in/3bdoelnaggar/"
+                    }
+                    styledImg {
+                        css {
+                            margin = "25px"
+                        }
 
+                        attrs {
+                            width = "100"
+                            height = "100"
+                            src = "./linkedin.png"
                         }
                     }
                 }
 
 
-                styledImg {
-                    css {
-                        margin="25px"
-
-                    }
-
+                a {
                     attrs {
-                        width = "100"
-                        height = "100"
-                        src = "./twitter.png"
-                        onClickFunction = {
-                            window.alert("Twitter")
+                        href = "https://twitter.com/3bdoelnaggar/"
+                    }
+                    styledImg {
+                        css {
+                            margin = "25px"
+
+                        }
+
+                        attrs {
+                            width = "100"
+                            height = "100"
+                            src = "./twitter.png"
 
                         }
                     }
                 }
-                styledImg {
-                    css {
-                        margin="25px"
-
-                    }
+                a {
                     attrs {
-                        width = "100"
-                        height = "100"
-                        src = "./github.png"
-                        onClickFunction = {
-                            window.alert("Twitter")
+                        href = "https://github.com/3bdoelnaggar/"
+                    }
+                    styledImg {
+                        css {
+                            margin = "25px"
+
+                        }
+                        attrs {
+                            width = "100"
+                            height = "100"
+                            src = "./github.png"
 
                         }
                     }
+                }
+                a {
+                    attrs {
+                        href = "https://play.google.com/store/apps/developer?id=Abdalla+Elnaggar"
+                    }
+                    styledImg {
+                        css {
+                            margin = "25px"
+
+                        }
+                        attrs {
+                            width = "100"
+                            height = "100"
+                            src = "./playstore.png"
+
+                        }
+                    }
+                }
+                styledP {
+                    css {
+                        position = Position.absolute
+                        bottom = 0.px
+                        right = 0.px
+                        textAlign = TextAlign.right
+                        margin="20px"
+                    }
+                    +"powered by kotlinJS"
                 }
             }
         }
